9be2c7f65953a52c5b64030f85e31a8b6f959c4c
Daniel Arango <daniel.arango@student.ie.edu>
2022-03-25 21:35:59 +0100

    fsaefs

--- main.py
+++ main.py
@@ -358,4 +358,4 @@ def planning_kpis():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3306)))
