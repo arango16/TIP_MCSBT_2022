2dc7d9c88eb48f29f709c7bf737917b8bd75a0e8
Daniel Arango <daniel.arango@student.ie.edu>
2022-03-26 21:47:53 +0100

    FINAL PUSH FOR REAL!

--- main.py
+++ main.py
@@ -402,6 +402,6 @@ def planning_kpis():
 def under_construction():
     return render_template("under_construction.html")
 
-app.run(debug=True)
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
